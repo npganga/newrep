1c84e7e069a11a2dede9a38bdd81225ae5643ea9
npganga <gangangpalayam@gmail.com>
2017-08-12 11:35:51 +0530

    Add files via upload

---
+++ hunter97.java
@@ -0,0 +1,10 @@
+package Hunter;
+
+public class hunter97 {
+public static void main(String[] args){
+	String barkOnce="smile";
+	String barkTwice="cries";
+	String barkThrice="fear";
+	System.out.println("Alex needs to wake up morning with "+barkThrice+" and "+barkTwice+" before he goes to school and returns home from school with "+barkOnce);
+}
+}
